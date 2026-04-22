1dc7b8c021770261f4f7de3a4a40b3c85ac43f76
https://github.com/Shreejad123 <shreejadkotian00@gmail.com>
2026-04-22 21:53:13 +0530

    update styling of login page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import Login from "./components/loginPage.jsx";
+import "./App.css";
 function App() {
   return (
     <>
--- my-react-app/src/components/loginPage.jsx
+++ my-react-app/src/components/loginPage.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./login.module.css";
-import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { IoEyeOutline } from "react-icons/io5";
+import { FiEyeOff } from "react-icons/fi";
+import { FaGoogle } from "react-icons/fa";
+import loginImage from "../assets/images/bg.jpg";
+import { GiStripedSun } from "react-icons/gi";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -36,73 +40,100 @@ const Login = () => {
     <>
       <div>
         <div className={styles.container}>
-          <div className={styles.loginContainer}>
-            <div className={styles.card}>
-              <h4>Get Everything you want</h4>
+          <div className={styles.imageCard}>
+            <img src={loginImage} alt="Background" className={styles.image} />
+            <div className={styles.text}>
+              <p className={styles.topLefttext}>
+                <span>
+                  Wise Quote
+                  <hr />
+                </span>
+              </p>
+              <div className={styles.paragraphtext}>
+                <h2>
+                  Get <br></br>Everything <br></br>You want
+                </h2>
+                <p>
+                  You Can get Everything you want if you work hard.<br></br>
+                  trust the process and stick to plan
+                </p>
+              </div>
             </div>
-            <div className={styles.loginCard}>
-              <h1>Welcome Back</h1>
-              <p>Enter your email and password to access your account</p>
-              <form onSubmit={handleSubmit} className={styles.loginForm}>
-                <div>
-                  <label>Email</label>
-                  <input
-                    className={styles.input}
-                    type="email"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+          </div>
+          <div className={styles.loginContainer}>
+            <div>
+              Cogie <GiStripedSun />
+            </div>
+            <h1 className={styles.header}>Welcome Back</h1>
+            <p className={styles.paragraph}>
+              Enter your email and password to access your account
+            </p>
+            <form onSubmit={handleSubmit} className={styles.loginForm}>
+              <div>
+                <label>Email</label>
+                <input
+                  className={styles.input}
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {errors.email && <p className="errorMessage">{errors.email}</p>}
+              </div>
+              <div className={styles.passwordWrapper}>
+                <label>Password</label>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className={styles.input}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {showPassword ? (
+                  <FiEyeOff
+                    className={styles.eyeIcon}
+                    onClick={() => setShowPassword((prevState) => !prevState)}
                   />
-                  {errors.email && (
-                    <p className="errorMessage">{errors.email}</p>
-                  )}
-                </div>
-                <div className={styles.passwordWrapper}>
-                  <label>Password</label>
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    className={styles.input}
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                ) : (
+                  <IoEyeOutline
+                    className={styles.eyeIcon}
+                    onClick={() => setShowPassword((prevState) => !prevState)}
                   />
-                  {showPassword ? (
-                    <AiFillEye
-                      className={styles.eyeIcon}
-                      onClick={() => setShowPassword((prevState) => !prevState)}
-                    />
-                  ) : (
-                    <AiFillEyeInvisible
-                      className={styles.eyeIcon}
-                      onClick={() => setShowPassword((prevState) => !prevState)}
-                    />
-                  )}
-                  {errors.password && (
-                    <p style={{ color: "red" }}>{errors.password}</p>
-                  )}
-                </div>
+                )}
+                {errors.password && (
+                  <p style={{ color: "red" }}>{errors.password}</p>
+                )}
+              </div>
+              <div className={styles.checkBoxforgotpassword}>
                 <div className={styles.checkbox}>
                   <input type="checkbox" />
-                  <label>Remember me:</label>
+                  <label>Remember me</label>
                 </div>
+                <div className={styles.forgotpassword}>
+                  <a>Forgot password</a>
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className={`btn btn-dark ${styles.loginBtn}`}
+                >
+                  Sign up
+                </button>
                 <div>
                   <button
-                    type="submit"
-                    className={`btn btn-dark ${styles.loginBtn}`}
+                    type="button"
+                    className={`btn btn-light ${styles.loginBtn}`}
                   >
-                    Sign up
+                    <FaGoogle />
+                    Signup with Google
                   </button>
-                  <div>
-                    <button
-                      type="button"
-                      className={`btn btn-light ${styles.loginBtn}`}
-                    >
-                      Signup with Google
-                    </button>
+                  <div className={styles.signup}>
+                    Don't have an account? Sign up
                   </div>
                 </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
